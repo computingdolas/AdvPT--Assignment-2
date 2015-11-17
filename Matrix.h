3769c3ad1673f24c1e5a03c13063d2348c229ea2
Sagar Dolas <computingdolas@gmail.com>
2015-11-17 17:24:52 +0100

    Final Changes Done

--- Matrix.h
+++ Matrix.h
@@ -1,89 +1,92 @@
+#pragma once
 
 #include <iostream>
 #include <fstream>
 #include <cassert>
+#include "Vector.h"
+#include "MatrixLike.h"
+
 using std::cout;
 using std::cin;
 using std::endl;
 using std::ostream;
 
-// this is a copy program and This is for second assignment 
-
-// this is version 2 
-
-// This is a copy program 
-template <class T>
-class Matrix 
+template <typename T>
+class Matrix : public MatrixLike<T , Matrix<T> >
 {
-	private :
+	private:
 	int row;
 	int col;
 	T *ptr;
-	
-	
+
+
 	public:
 	int getrow()
 	{
-		return row;	
+		return row;
 	}
-	
-	
-	Matrix(int nrow ,int ncol ,T ini);//Constructor 
-	~Matrix();//Destructor
-	Matrix(const Matrix &obj);// Copy constructor 
-	Matrix(int nrow ,int ncol);// Assignment Constructor 
-	
+
+
+	Matrix(int nrow ,int ncol ,T ini);//Constructor
+	~Matrix() override;//Destructor
+	Matrix(const Matrix &obj);// Copy constructor
+	Matrix(int nrow ,int ncol);// Assignment Constructor
 
 
 
-	Matrix& operator = (const Matrix& obj);//Assignment Operator 
+
+	Matrix& operator = (const Matrix& obj);//Assignment Operator
  	void display() const ;// Dislplay Function
- 
+
  	Matrix& operator+=(const Matrix& obj);
  	Matrix& operator-=(const Matrix& obj);
  	const T& operator()(int r, int c) const;
 	T& operator()(int r, int c ) ;
-	
-	
+
+
 	Matrix operator+(const Matrix& obj) const;
  	Matrix operator-(const Matrix& obj) const;
  	Matrix& operator*=(const Matrix& obj);
  	Matrix operator*(const Matrix& obj);
  	bool operator==(const Matrix& obj);
 
- 	bool operator!=(const Matrix& obj); 
- 	Matrix inverseDiagonal() const;
+ 	bool operator!=(const Matrix& obj);
+ 	Matrix inverseDiagonal() const override;
+
+ 	// Matrix Vector multiplication
+
+ 	Vector<T> operator*(const Vector<T>& obj) const override ;
 
 
 };
-template <class T>
-const T& Matrix<T>::operator()(int r, int c) const 
+template <typename T>
+const T& Matrix<T>::operator()(int r, int c) const
 {
    	return(*(ptr + r * col + c ));
 }
 
-template <class T>
-T& Matrix<T>:: operator()(int r, int c ) 
+template <typename T>
+T& Matrix<T>:: operator()(int r, int c )
 {
-	return (*(ptr +r * col + c));	
+	return (*(ptr +r * col + c));
 
 }
 
-template <class T>
+template <typename T>
 Matrix<T>::Matrix(int nrow, int ncol ,T ini)
 {
-	cout<<" constructor created "<<endl;
-	
+	//cout<<" constructor created "<<endl;
+
 	row= nrow ;
 	col= ncol ;
 	ptr = new T [row*col];
 	for (int i =0;i < row*col ;++i)
 		*(ptr+ i)= ini ;
 }
-template <class T>
+template <typename T>
 Matrix<T>::Matrix(int nrow, int ncol)
 {
-	int i;	
+	int i;
 	row= nrow;
 	col= ncol ;
 	ptr = new T [row * col ];
@@ -93,15 +96,15 @@ Matrix<T>::Matrix(int nrow, int ncol)
 }
 
 
-template <class T>
+template <typename T>
 Matrix<T>::~Matrix()
 {
 
-	cout<<" Destructor called"<<endl;
+	//cout<<" Destructor called"<<endl;
 	delete [] ptr;
-			
+
 }
-template <class T>
+template <typename T>
 Matrix<T>& Matrix<T>:: operator = (const Matrix& obj)
 {
 	cout<<"The Assignment copy constructor is created"<<endl;
@@ -111,13 +114,13 @@ Matrix<T>& Matrix<T>:: operator = (const Matrix& obj)
 		col = obj.col;
 		for (int i =0;i < row * col; ++i)
 			*(ptr+i)= *(obj.ptr + i);
-		
+
 	}
 
 	return *this ;
 }
 
-template <class T>
+template <typename T>
 Matrix<T>::Matrix(const Matrix &obj)
 
 {
@@ -127,9 +130,9 @@ Matrix<T>::Matrix(const Matrix &obj)
 	ptr = new T [row*col];
 	for (int j =0;j<row*col ;++j)
 		*(ptr+j)= *(obj.ptr +j);
-		
+
 }
-template <class T>
+template <typename T>
 Matrix<T>& Matrix<T>::operator+=(const Matrix& obj)
 {
 	cout<<" Assignment addition operator is called"<<endl;
@@ -142,7 +145,7 @@ Matrix<T>& Matrix<T>::operator+=(const Matrix& obj)
 	return(* this);
 }
 
-template <class T>
+template <typename T>
 Matrix<T>& Matrix<T>::operator-=(const Matrix& obj)
 {
 	cout<<" Assignment subtraction operator is called "<<endl;
@@ -156,7 +159,7 @@ Matrix<T>& Matrix<T>::operator-=(const Matrix& obj)
 }
 
 
-template <class T>
+template <typename T>
 Matrix<T> Matrix<T>::operator+(const Matrix& obj) const
 {
 	cout<<" the Assignment binary addition operator is created"<<endl;
@@ -167,9 +170,9 @@ Matrix<T> Matrix<T>::operator+(const Matrix& obj) const
 }
 
 
-template <class T>
+template <typename T>
 Matrix<T> Matrix<T>::operator-(const Matrix& obj) const
-{	
+{
 	cout<<" the Assignment binary substraction operator is created"<<endl;
 	Matrix temp(* this);
 	temp-=obj;
@@ -178,12 +181,12 @@ Matrix<T> Matrix<T>::operator-(const Matrix& obj) const
 }
 
 
-template <class T>
+template <typename T>
 Matrix<T>& Matrix<T>::operator*=(const Matrix& obj)
 {
 	 assert(this->col == obj.row);
-		
-		Matrix temp (* this );		
+
+		Matrix temp (* this );
 		cout<<"Assignment Matrix Mult is created"<<endl;
 		int j;
 		double sum;
@@ -194,23 +197,23 @@ Matrix<T>& Matrix<T>::operator*=(const Matrix& obj)
 		{
 			for ( j=0;j<colu;++j)
 			{
-			sum =0;	
+			sum =0;
 				for (int k=0;k< obj.row;++k)
 				{
-					
+
 					sum+= temp(i,k)* obj(k,j);
 				}
 				cout<<"the total sum is"<<sum<<endl;
-			*(m3.ptr +  i*(obj.col) +j) = sum;	
+			*(m3.ptr +  i*(obj.col) +j) = sum;
 			}
 		}
 		(*this) = m3;
 		return (*this);
-	
-	
+
+
 }
 
-template <class T>
+template <typename T>
 Matrix<T> Matrix<T>::operator*(const Matrix& obj)
 {
 	cout<<" The binary Assignment multiplication operator is initialised "<<endl;
@@ -220,19 +223,19 @@ Matrix<T> Matrix<T>::operator*(const Matrix& obj)
 }
 
 
-template <class T>
+template <typename T>
 bool Matrix<T>::operator==(const Matrix& obj)
 {
 	bool var =true ;
 	row= obj.row;
 	col= obj.col;
-	
+
 	if(this->row!=obj.row || this->col!=obj.col)
 	{
 		var =false;
-	}	
+	}
 	else
-	{	
+	{
 		for(int i =0; i <row* col ;++i)
 		{
 			if(*(ptr +i) != *(obj.ptr +i))
@@ -240,67 +243,83 @@ bool Matrix<T>::operator==(const Matrix& obj)
 			var=false;
 			break;
 			}
-			
+
 		}
-	}	
+	}
 	return var;
 }
 
-template <class T>
+template <typename T>
 bool Matrix<T>:: operator !=(const Matrix& obj)
  {
- 	
+
  	if(*this == obj)
  		return false;
  	else
- 		return true;	
- } 
+ 		return true;
+ }
 
 
-template <class T>
-void Matrix<T>::display() const 
+template <typename T>
+void Matrix<T>::display() const
 {
-	Matrix m3 = (* this);	
+	Matrix m3 = (* this);
 	cout<<" The contents of the matrix are being displayed "<<endl;
 	int i ;
 	int j;
 		for (i=0;i<row;++i)
-		{		
-			cout<<endl;		
+		{
+			cout<<endl;
 			for (j=0; j<col;++j)
 			//cout<<i<<"  "<<j<<endl;
 			cout<<(m3(i,j))<<"\t\t";
-			
+
 		}
-		
+
 }
-template<class T>
+template<typename T>
 Matrix<T> Matrix<T>::inverseDiagonal() const
 {
-	
+
 	assert(this->row == this->col);
 	Matrix m3(this->row,this->col,0);
 	int i=0;
 	int j=0;
-	for (i=0;i<this->row;++i)
+	for (i=0;i<(this->row);++i)
 	{
-		j = i;	
+		j = i;
 		m3(i,j)= 1/ (*this)(i,j);
-			
+
 	}
 	return m3;
 
 }
 
-template <class T>
-ostream& operator<<(ostream& os,const Matrix<T>& obj ) 
+template <typename T>
+ostream& operator<<(ostream& os,const Matrix<T>& obj )
 {
-	
-	
-	
 	obj.display();
 	return os;
 
-	
+
 }
 
+template <typename T>
+Vector<T> Matrix<T>::operator*(const Vector<T>& obj) const
+{
+
+	//assert(this->col = obj.size());
+	int num = obj.size();
+	Vector<T> v(num);
+
+	for (int i =0 ; i <this->row; ++i)
+
+		{
+			for (int j =0 ; j< this->col ; ++j )
+				v(i)+= (*this)(i,j) * obj(j);
+
+		}
+
+
+	return v;
+}
--- README.md
+++
@@ -1,3 +0,0 @@
-# AdvPT--Assignment-2
-
-This is Assignment 2 .....and we hare developing 1D Poisson Equation Solver in C++ 
